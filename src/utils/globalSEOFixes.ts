a7007bb7964618b36edf9dad67db2b96b70d8ab5
nicovillamil7 <158379966+nicovillamil7@users.noreply.github.com>
2025-07-02 00:40:07 +0000

    Restored to '91530da3ca24a193bacad222e8fe23ed6c0e4e37'
    Replit-Restored-To: 91530da3ca24a193bacad222e8fe23ed6c0e4e37

--- src/utils/globalSEOFixes.ts
+++ src/utils/globalSEOFixes.ts
@@ -86,7 +86,7 @@ export class GlobalSEOOptimizer {
     if (!document.querySelector('meta[name="twitter:site"]')) {
       const twitterSite = document.createElement('meta');
       twitterSite.name = 'twitter:site';
-      twitterSite.content = '@genesisstonefl';
+      twitterSite.content = '@genesisstoneusa';
       document.head.appendChild(twitterSite);
     }
 
